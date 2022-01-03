c035879f8493149e4bb4bc80c646779e67d63938
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-01-03 12:12:06 +0000

    Add fp

--- apps/web-app/index.d.ts
+++ apps/web-app/index.d.ts
@@ -1,12 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Theme as ComponentTheme } from "@tnmw/components"
+
 declare module '*.svg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const ReactComponent: any;
   export default content;
 }
 
 declare module '@emotion/react' {
-  export interface Theme extends ComponentTheme {
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Theme extends ComponentTheme {}
 }
