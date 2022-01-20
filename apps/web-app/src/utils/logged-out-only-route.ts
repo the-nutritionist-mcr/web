29eb643a38a2267f4ce4f06ca7a480e0f8841356
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-01-20 12:21:02 +0000

    fix type error

--- apps/web-app/src/utils/logged-out-only-route.ts
+++ apps/web-app/src/utils/logged-out-only-route.ts
@@ -6,7 +6,7 @@ export const loggedOutOnlyRoute = (
   redirectTo: string,
   serverSidePropsCallback?: GetServerSideProps
 ): GetServerSideProps => {
-  return async (context) => {
+  return async context => {
     const tokenPair = Object.entries(context.req.cookies).find(([key]) =>
       key.endsWith('.accessToken')
     );
@@ -15,7 +15,7 @@ export const loggedOutOnlyRoute = (
       return (await serverSidePropsCallback?.(context)) ?? { props: {} };
     }
 
-    const verifyResult = await verifyJwtToken(tokenPair[1]);
+    const verifyResult = await verifyJwtToken({ token: tokenPair[1] });
 
     if (verifyResult.isValid) {
       return backendRedirect(
