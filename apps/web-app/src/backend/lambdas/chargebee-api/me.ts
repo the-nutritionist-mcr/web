d4523ee5932eb48f5f992c3c09f1c53dfde3f7af
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-02 21:46:28 +0000

    remove logging

--- apps/web-app/src/backend/lambdas/chargebee-api/me.ts
+++ apps/web-app/src/backend/lambdas/chargebee-api/me.ts
@@ -1,22 +1,45 @@
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import { ChargeBee } from 'chargebee-typescript';
 
-import { authorise } from '../data-api/authorise';
+const chargebee = new ChargeBee();
+
+chargebee.configure({
+  site: 'thenutritionist-test',
+  api_key: 'test_Tv0orhs9B9cFhIWTl6UKqT8ej1ltFZdo',
+});
+
+// import { authorise } from '../data-api/authorise';
 
 import { returnErrorResponse } from '../data-api/return-error-response';
 
-export const handler: APIGatewayProxyHandlerV2 = async event => {
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   try {
-    authorise(event, ['admin']);
+    // authorise(event, ['admin']);
+
+    const result = await new Promise((accept, reject) => {
+      chargebee.customer.list().request((error, result) => {
+        if (error) {
+          reject(error);
+        }
+        accept(result);
+      });
+    });
+
+    // @ts-ignore
+    result.list.forEach((item) => console.log(item.customer));
 
     return {
       statusCode: 200,
       body: JSON.stringify({}),
       headers: {
         'access-control-allow-origin': '*',
-        'access-control-allow-headers': '*'
-      }
+        'access-control-allow-headers': '*',
+      },
     };
   } catch (error) {
     return returnErrorResponse(error);
   }
 };
+
+// @ts-ignore
+handler();
--- apps/web-app/src/backend/lambdas/data-api/get.ts
+++ apps/web-app/src/backend/lambdas/data-api/get.ts
@@ -6,23 +6,21 @@ import { authorise } from './authorise';
 
 import { returnErrorResponse } from './return-error-response';
 
-export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+export const handler: APIGatewayProxyHandlerV2 = async event => {
   try {
-    // eslint-disable-next-line unicorn/no-null
-    console.log(JSON.stringify(event, null, 2));
     authorise(event, ['admin']);
 
     const dynamodb = new DynamoDBClient({});
     const client = DynamoDBDocumentClient.from(dynamodb);
 
     const command = new ScanCommand({
-      TableName: process.env['DYNAMODB_TABLE'],
+      TableName: process.env['DYNAMODB_TABLE']
     });
 
     const { Items: items } = await client.send(command);
 
     const body = JSON.stringify({
-      items: items.filter((item) => !item.deleted),
+      items: items.filter(item => !item.deleted)
     });
 
     return {
@@ -30,8 +28,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
       body,
       headers: {
         'access-control-allow-origin': '*',
-        'access-control-allow-headers': '*',
-      },
+        'access-control-allow-headers': '*'
+      }
     };
   } catch (error) {
     return returnErrorResponse(error);
