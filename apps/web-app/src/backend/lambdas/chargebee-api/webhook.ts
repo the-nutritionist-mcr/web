d07debee3180fc448a2460ba8eb7679eaca579a3
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-03 08:38:51 +0000

    log webhook event

--- apps/web-app/src/backend/lambdas/chargebee-api/webhook.ts
+++ apps/web-app/src/backend/lambdas/chargebee-api/webhook.ts
@@ -4,6 +4,7 @@ import { returnErrorResponse } from '../data-api/return-error-response';
 
 export const handler: APIGatewayProxyHandlerV2 = async event => {
   try {
+    console.log(JSON.stringify(event))
     // Not yet implemented
   } catch (error) {
     return returnErrorResponse(error);
