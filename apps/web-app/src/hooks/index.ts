56f92a5c49e40d97930e1f80216ae728e70f5ea5
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-03 19:48:19 +0000

    added endpoint

--- apps/web-app/src/hooks/index.ts
+++ apps/web-app/src/hooks/index.ts
@@ -4,3 +4,10 @@ import { Exclusion, Recipe } from '@tnmw/admin-app';
 export const useCustomisations = () => useResource<Exclusion>('customisation');
 
 export const useRecipes = () => useResource<Recipe>('recipe');
+
+interface Me {
+  id: string;
+  first_name: string;
+}
+
+export const useMe = () => useResource<Me>('customers/me');
