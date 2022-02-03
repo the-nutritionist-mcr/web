dd50b1eb85e2dc330525b22dbd20051a8cfdbc97
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-03 08:17:52 +0000

    fix failed linting

--- apps/web-app/src/backend/lambdas/chargebee-api/me.ts
+++ apps/web-app/src/backend/lambdas/chargebee-api/me.ts
@@ -1,45 +1,29 @@
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
-import { ChargeBee } from 'chargebee-typescript';
+// import { ChargeBee } from 'chargebee-typescript';
 
-const chargebee = new ChargeBee();
+// const chargebee = new ChargeBee();
 
-chargebee.configure({
-  site: 'thenutritionist-test',
-  api_key: 'test_Tv0orhs9B9cFhIWTl6UKqT8ej1ltFZdo',
-});
+// chargebee.configure({
+//   site: 'thenutritionist-test',
+//   api_key: 'test_Tv0orhs9B9cFhIWTl6UKqT8ej1ltFZdo',
+// });
 
-// import { authorise } from '../data-api/authorise';
+import { authorise } from '../data-api/authorise';
 
 import { returnErrorResponse } from '../data-api/return-error-response';
 
-export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+export const handler: APIGatewayProxyHandlerV2 = async event => {
   try {
-    // authorise(event, ['admin']);
-
-    const result = await new Promise((accept, reject) => {
-      chargebee.customer.list().request((error, result) => {
-        if (error) {
-          reject(error);
-        }
-        accept(result);
-      });
-    });
-
-    // @ts-ignore
-    result.list.forEach((item) => console.log(item.customer));
-
+    authorise(event, ['admin']);
     return {
       statusCode: 200,
       body: JSON.stringify({}),
       headers: {
         'access-control-allow-origin': '*',
-        'access-control-allow-headers': '*',
-      },
+        'access-control-allow-headers': '*'
+      }
     };
   } catch (error) {
     return returnErrorResponse(error);
   }
 };
-
-// @ts-ignore
-handler();
