3b19c77de7b6578d40f4733fa3bb5e1149ae7a93
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-04 17:19:50 +0000

    stuff

--- apps/web-app/src/pages/admin/admin-template.tsx
+++ apps/web-app/src/pages/admin/admin-template.tsx
@@ -1,27 +1,19 @@
 import { Grommet } from 'grommet';
 import { FC } from 'react';
-import Head from 'next/head';
 
 const theme = {
   global: {
     font: {
       family: 'Roboto',
       size: '14pt',
-      height: '20px',
-    },
-  },
+      height: '20px'
+    }
+  }
 };
 
-export const AdminTemplate: FC = (props) => (
+export const AdminTemplate: FC = props => (
   <Grommet theme={theme}>
-    <Head>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-      />
-    </Head>
-    {props.children}
-  </Grommet>
+  {props.children}</Grommet>
 );
 
 export default AdminTemplate;
--- apps/web-app/src/utils/swr-fetcher.ts
+++ apps/web-app/src/utils/swr-fetcher.ts
@@ -1,1 +1,44 @@
+import { getOutputs } from '../aws/get-outputs';
+import { currentUser } from '../aws/authenticate';
 
+const getFetchInit = async (init?: RequestInit) => {
+  const user = await currentUser();
+  if (!user) {
+    return {};
+  }
+  const {
+    signInUserSession: {
+      accessToken: { jwtToken },
+    },
+  } = user;
+
+  const withToken = {
+    headers: {
+      authorization: jwtToken,
+    },
+  };
+
+  return init ? { ...init, ...withToken } : withToken;
+};
+
+export const swrFetcher = async <T>(path: string, init?: RequestInit): Promise<T> => {
+  const { ApiDomainName: domainName } = await getOutputs();
+
+  console.log(domainName);
+
+  const finalInit = await getFetchInit(init);
+  const fullPath = `https://${domainName}/${path}`;
+  const response = await fetch(fullPath, finalInit);
+
+  const data = await response.json();
+
+  console.log(data);
+
+  if (!response.ok) {
+    const error = new Error(
+      `Tried to make a request to ${fullPath} but the server returned a ${response.status} status code with the message "${data.error}"`
+    );
+    throw error;
+  }
+  return await response.json();
+};
