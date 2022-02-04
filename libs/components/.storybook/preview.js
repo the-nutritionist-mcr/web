6c6e4693743bdc90c7430bc3318b99f009a08a15
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-04 18:37:24 +0000

    fixed

--- libs/components/.storybook/preview.js
+++ libs/components/.storybook/preview.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { ThemeProvider } from '@emotion/react';
 
 import {
--- libs/components/src/organisms/account/account.stories.tsx
+++ libs/components/src/organisms/account/account.stories.tsx
@@ -1,29 +1,30 @@
-
 import { Story, Meta } from '@storybook/react';
 import { FC } from 'hoist-non-react-statics/node_modules/@types/react';
 
 import { Account as AccountComponent } from './account';
 
-type PropsOfFC<T> = T extends FC<infer P> ? P : never
+type PropsOfFC<T> = T extends FC<infer P> ? P : never;
 
 export default {
   title: 'organisms/Account',
-  component: AccountComponent,
+  component: AccountComponent
 } as Meta;
 
 const initialDetails = {
-  firstName: "Ben",
-  lastName: "Wainwright",
-  email: "ben@thenutritionistmcr.com",
-  contactNumber: "0123456789",
-  addressLine1: "Flat 2",
-  addressLine2: "Rectangle house",
-  addressLine3: "Salford",
-  city: "Manchester",
-  country: "United Kingdom",
-  postCode: "M1 5LT"
-}
+  firstName: 'Ben',
+  lastName: 'Wainwright',
+  email: 'ben@thenutritionistmcr.com',
+  contactNumber: '0123456789',
+  addressLine1: 'Flat 2',
+  addressLine2: 'Rectangle house',
+  addressLine3: 'Salford',
+  city: 'Manchester',
+  country: 'United Kingdom',
+  postcode: 'M1 5LT'
+};
 
-const Template: Story<PropsOfFC<typeof AccountComponent>> = (args) => <AccountComponent {...args} userDetails={initialDetails}/>;
+const Template: Story<PropsOfFC<typeof AccountComponent>> = args => (
+  <AccountComponent {...args} userDetails={initialDetails} />
+);
 
 export const Account = Template.bind({});
