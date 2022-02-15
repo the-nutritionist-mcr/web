b158d45474063d626d672459ba8a96e5f794decd
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-02-15 16:00:05 +0000

    added plans to me endpoint

--- apps/web-app/src/backend/lambdas/chargebee-api/me.ts
+++ apps/web-app/src/backend/lambdas/chargebee-api/me.ts
@@ -1,6 +1,7 @@
 import { ENV } from '../../../infrastructure/constants';
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
 import { ChargeBee } from 'chargebee-typescript';
+import { CHARGEBEE } from "@tnmw/constants"
 
 const chargebee = new ChargeBee();
 
@@ -12,30 +13,66 @@ chargebee.configure({
 import { authorise } from '../data-api/authorise';
 import { returnErrorResponse } from '../data-api/return-error-response';
 
+const getPlansForCustomer = async (id: string) => {
+  const { list } = await chargebee.subscription.list({customer_id: {is: id}}).request()
+
+  const plans = await Promise.all(list.map(async entry => {
+
+    const planSubscriptionItem = entry.subscription.subscription_items.filter(item => item.item_type === CHARGEBEE.itemTypes.plan)
+    if(planSubscriptionItem.length !== 1) {
+      return
+    }
+
+    const plan = await chargebee.plan.retrieve(entry.subscription.plan_id).request()
+    const itemPrice = chargebee.item_price.retrieve(planSubscriptionItem[0].item_price_id).request()
+
+    const daysPerWeek = Number(plan[CHARGEBEE.customFields.plan.daysPerWeek])
+    const itemsPerDay = Number(plan[CHARGEBEE.customFields.plan.itemsPerDay])
+    const totalMeals = daysPerWeek * itemsPerDay
+
+    const { name } = (await itemPrice).item_family
+    return { 
+      name,
+      daysPerWeek,
+      itemsPerDay,
+      totalMeals
+    }
+  }))
+
+  return plans.filter(Boolean)
+}
+
+const getCustomerByid = async (id: string) => {
+  const customerPromise = chargebee.customer.retrieve(id).request()
+  const plans = await getPlansForCustomer(id)
+
+  const { first_name, last_name, email, billing_address, phone } = (await customerPromise).customer
+  const { line1, line2, line3, city, country, zip: postcode } = billing_address ?? {};
+
+  return {
+          plans,
+          phone,
+          first_name,
+          last_name,
+          email,
+          address_line1: line1,
+          address_line2: line2,
+          address_line3: line3,
+          city,
+          country,
+          postcode
+  }
+}
+
+
 export const handler: APIGatewayProxyHandlerV2 = async event => {
   try {
     const { username } = await authorise(event);
-
-    const result = await chargebee.customer.retrieve(username).request();
-
-    const { first_name, last_name, email, billing_address, phone } =
-      result.customer;
-    const { line1, line2, line3, city, country, zip: postcode } = billing_address ?? {};
+    const customer = await getCustomerByid(username);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        phone,
-        first_name,
-        last_name,
-        email,
-        address_line1: line1,
-        address_line2: line2,
-        address_line3: line3,
-        city,
-        country,
-        postcode
-      }),
+      body: JSON.stringify(customer),
       headers: {
         'access-control-allow-origin': '*',
         'access-control-allow-headers': '*'
--- libs/constants/src/index.ts
+++ libs/constants/src/index.ts
@@ -1,6 +1,27 @@
 export const E2E = {
   testEmail: 'ben+e2etesting@thenutritionistmcr.com',
-  testPassword: 'the-cypress-test-password'
+  testPassword: 'the-cypress-test-password',
+};
+
+export const CHARGEBEE = {
+  itemTypes: {
+    plan: 'plan',
+  },
+  customFields: {
+    customer: {
+      customerProfileNotes: 'cf_customer_profile_notes',
+      deliveryDay1: 'cf_cook_1_delivery_day',
+      deliveryDay2: 'cf_cook_2_delivery_day',
+      deliveryDay3: 'cf_cook_3_delivery_day',
+    },
+    plan: {
+      itemsPerDay: 'cf_items_per_day',
+      daysPerWeek: 'cf_days_per_week',
+    },
+    itemFamily: {
+      isExtra: 'cf_extra',
+    },
+  },
 };
 
 export const IAM = {
@@ -9,15 +30,15 @@ export const IAM = {
       adminGetUser: 'cognito-idp:AdminGetUser',
       adminCreateUser: 'cognito-idp:AdminCreateUser',
       adminDeleteUser: 'cognito-idp:AdminDeleteUser',
-      adminSetUserPassword: 'cognito-idp:AdminSetUserPassword'
-    }
-  }
+      adminSetUserPassword: 'cognito-idp:AdminSetUserPassword',
+    },
+  },
 } as const;
 
 export const TNM_WEB_LOCALSTORAGE_KEY = 'tnm-web-cache';
 
 export const NODE_OPTS = {
-  EnableSourceMaps: '--enable-source-maps'
+  EnableSourceMaps: '--enable-source-maps',
 } as const;
 
 export const MAILSLURP_INBOX =
@@ -33,31 +54,31 @@ export const ENV = {
     ChargeBeeToken: 'CHARGEBEE_TOKEN',
     ChargeBeeSite: 'CHARGEBEE_SITE',
     ChargeBeeWebhookUsername: 'CHARGEBEE_WEBHOOK_USERNAME',
-    ChargeBeeWebhookPasssword: 'CHARGEBEE_WEBHOOK_PASSWORD'
-  }
+    ChargeBeeWebhookPasssword: 'CHARGEBEE_WEBHOOK_PASSWORD',
+  },
 } as const;
 
 export const CHARGEBEE_SITES = {
-  test: 'thenutritionist-test'
+  test: 'thenutritionist-test',
 } as const;
 
 export const RESOURCES = {
   Recipe: 'recipe',
   Customisation: 'customisation',
-  CookPlan: 'cook-plan'
+  CookPlan: 'cook-plan',
 } as const;
 
 export const HTTP = {
   statusCodes: {
     Forbidden: 403,
     Ok: 200,
-    InternalServerError: 500
+    InternalServerError: 500,
   },
   headerNames: {
     ContentType: 'Content-Type',
     XAmxDate: 'X-Amz-Date',
     Authorization: 'Authorization',
-    XApiKey: 'X-Api-Key'
+    XApiKey: 'X-Api-Key',
   },
   verbs: {
     Post: 'POST',
@@ -65,11 +86,11 @@ export const HTTP = {
     Put: 'PUT',
     Options: 'OPTIONS',
     Patch: 'PATCH',
-    Delete: 'DELETE'
-  }
+    Delete: 'DELETE',
+  },
 } as const;
 
 export const USER_ATTRIBUTES = {
   ChargebeeId: 'chargebeeId',
-  UserCustomisations: 'userCustomisations'
+  UserCustomisations: 'userCustomisations',
 } as const;
