8831724f205b84afebe62aa557ecc02262930537
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-19 09:56:12 +0100

    changed DNS

--- apps/web-app/src/infrastructure/get-domain-name.ts
+++ apps/web-app/src/infrastructure/get-domain-name.ts
@@ -1,9 +1,13 @@
 export const BASE_DOMAIN_NAME = 'thenutritionistmcr.com';
 
+const PROD_NAME = 'account';
+
 export const getDomainName = (environment: string, prefix?: string) => {
   const otherPrefix = prefix ? `${prefix}.` : ``;
   const finalPrefix =
-    environment !== 'prod' ? `${otherPrefix}${environment}.` : otherPrefix;
+    environment !== 'prod'
+      ? `${otherPrefix}${environment}.`
+      : `${otherPrefix}${PROD_NAME}.`;
 
-  return `${finalPrefix}app.${BASE_DOMAIN_NAME}`;
+  return `${finalPrefix}${BASE_DOMAIN_NAME}`;
 };
