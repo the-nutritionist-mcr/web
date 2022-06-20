8b76244d1aed0491412437e11f9285d372c287f4
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-20 20:17:31 +0100

    test

---
+++ apps/web-app/src/pages/pricing.page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import { Pricing as PricingComponent } from '@tnmw/static-pages';
+
+const OurStory: FC = () => {
+  return <PricingComponent />;
+};
+
+export default OurStory;
