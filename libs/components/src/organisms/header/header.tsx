5e94b840f9df8aa6f9bfadc985f7a6642565abac
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-22 18:38:43 +0100

    fix the header on mobileg

--- libs/components/src/organisms/header/header.tsx
+++ libs/components/src/organisms/header/header.tsx
@@ -1,15 +1,27 @@
 import { useBreakpoints } from '../../hooks';
 import styled from '@emotion/styled';
 import { useTheme } from '@emotion/react';
-import { FC } from 'react';
 
 import DesktopHeader from './desktop-header';
 import MobileHeader from './mobile-header';
 import { AdminNav } from './admin-nav';
 
-const SiteNavbar = styled('nav')`
+const SiteNavbarDesktop = styled('nav')`
   display: flex;
   flex-direction: column;
+  font-family: 'Acumin Pro', Arial, sans-serif;
+  font-weight: 700;
+  height: ${(props) => props.theme.menubarHeight}px;
+  padding: 0 30px;
+  border-bottom: 1px solid black;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: white;
+`;
+
+const SiteNavbarMobile = styled('nav')`
+  display: flex;
   font-family: 'Acumin Pro', Arial, sans-serif;
   font-weight: 700;
   height: ${(props) => props.theme.menubarHeight}px;
@@ -28,17 +40,15 @@ interface HeaderProps {
 const Header = (props: HeaderProps) => {
   const theme = useTheme();
   const currentBreakpoint = useBreakpoints(theme.breakpoints);
-  return (
-    <SiteNavbar>
-      {currentBreakpoint === 'large' ? (
-        <>
-          <DesktopHeader />
-          {props.admin && <AdminNav />}
-        </>
-      ) : (
-        <MobileHeader />
-      )}
-    </SiteNavbar>
+  return currentBreakpoint === 'large' ? (
+    <SiteNavbarDesktop>
+      <DesktopHeader />
+      {props.admin && <AdminNav />}
+    </SiteNavbarDesktop>
+  ) : (
+    <SiteNavbarMobile>
+      <MobileHeader />
+    </SiteNavbarMobile>
   );
 };
 
