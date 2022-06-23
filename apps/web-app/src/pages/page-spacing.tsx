ccec35cebe5103911f4429b70c8103272752bfbe
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-06-23 19:49:53 +0100

    fix spacing

--- apps/web-app/src/pages/page-spacing.tsx
+++ apps/web-app/src/pages/page-spacing.tsx
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 
 const Container = styled.section`
   padding-top: 3rem;
+  display: flex;
+  width: 100%;
+  padding: 0 auto;
+  align-items: center;
+  flex-direction: column;
 `;
 
 interface PageSpacingProps {
