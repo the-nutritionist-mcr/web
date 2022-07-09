ed38d239a61f2bee254e217247a6ff3039fa7a46
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-07-09 19:06:17 +0100

    swallow non existence

--- apps/web-app-e2e/src/support/delete-chargebee-customer.ts
+++ apps/web-app-e2e/src/support/delete-chargebee-customer.ts
@@ -2,22 +2,27 @@ import { ChargeBee } from 'chargebee-typescript';
 import { CHARGEBEE, ENV } from '@tnmw/constants';
 
 export const deleteChargebeeCustomer = async (id: string) => {
-  const chargebee = new ChargeBee();
-  const key = process.env[`NX_${ENV.varNames.ChargeBeeToken}`];
-  chargebee.configure({
-    site: CHARGEBEE.sites.test,
-    api_key: key,
-  });
-
-  await new Promise((accept, reject) => {
-    chargebee.customer.delete(id).request(function (error, result) {
-      if (error) {
-        reject(error);
-      } else {
-        accept(result);
-      }
+  try {
+    const chargebee = new ChargeBee();
+    const key = process.env[`NX_${ENV.varNames.ChargeBeeToken}`];
+    chargebee.configure({
+      site: CHARGEBEE.sites.test,
+      api_key: key,
     });
-  });
 
-  return null;
+    await new Promise((accept, reject) => {
+      chargebee.customer.delete(id).request(function (error, result) {
+        if (error) {
+          reject(error);
+        } else {
+          accept(result);
+        }
+      });
+    });
+
+    return null;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 };
