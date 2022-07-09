26808ac6d01bb82199041eea61459a27b7106af7
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-07-09 17:51:40 +0100

    put other tests back

--- apps/web-app-e2e/e2e/initial/login.cy.ts
+++ apps/web-app-e2e/e2e/initial/login.cy.ts
@@ -5,32 +5,32 @@ describe('The login page', () => {
     cy.task('deleteCognitoUser', E2E.nonExistingUser.username);
   });
 
-  // it('Should load a page with a login form', () => {
-  //   cy.visit('/login/');
-  //   cy.get('form').find('button').contains('Login');
-  // });
+  it('Should load a page with a login form', () => {
+    cy.visit('/login/');
+    cy.get('form').find('button').contains('Login');
+  });
 
-  // it("Should display an error message if the user doesn't exist", () => {
-  //   cy.visit('/login/');
-  //   cy.get('form').should('be.visible');
-  //   cy.get('form').find("input[name='email']").clear().type('a@b.c');
-  //   cy.get('form').find("input[name='password']").clear().type('asdds');
-  //   cy.get('form').find('button').contains('Login').click();
-  //   cy.get('form').contains('User does not exist');
-  // });
+  it("Should display an error message if the user doesn't exist", () => {
+    cy.visit('/login/');
+    cy.get('form').should('be.visible');
+    cy.get('form').find("input[name='email']").clear().type('a@b.c');
+    cy.get('form').find("input[name='password']").clear().type('asdds');
+    cy.get('form').find('button').contains('Login').click();
+    cy.get('form').contains('User does not exist');
+  });
 
-  // it('Should display an error message if your password is incorrect', () => {
-  //   cy.visit('/login/');
-  //   cy.get('form').should('be.visible');
-  //   cy.get('form')
-  //     .find("input[name='email']")
-  //     .clear()
-  //     .type(E2E.adminUserOne.email);
+  it('Should display an error message if your password is incorrect', () => {
+    cy.visit('/login/');
+    cy.get('form').should('be.visible');
+    cy.get('form')
+      .find("input[name='email']")
+      .clear()
+      .type(E2E.adminUserOne.email);
 
-  //   cy.get('form').find("input[name='password']").clear().type('asdsdfasd');
-  //   cy.get('form').find('button').contains('Login').click();
-  //   cy.get('form').contains('Incorrect username or password');
-  // });
+    cy.get('form').find("input[name='password']").clear().type('asdsdfasd');
+    cy.get('form').find('button').contains('Login').click();
+    cy.get('form').contains('Incorrect username or password');
+  });
 
   it('Creating an account on ChargeBee should provide an account that I can log in with', () => {
     cy.task('createChargebeeCustomer');
