bec27bc2ef7af675b59390da824f44a0d1a552f7
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-07-12 18:48:11 +0100

    Add list of approved testers

---
+++ apps/web-app/src/backend/lambdas/chargebee-api/approved-testers.ts
@@ -0,0 +1,11 @@
+export const approvedTesters = [
+  'bwainwright28@gmail.com',
+  //   'marc.odonnell@icloud.com',
+  //   'peter_barnes@live.co.uk',
+  //   'james.fowler@j2recruitment.co.uk',
+  //   'etiennejr87@gmail.com',
+  //   'pchemer@gmail.com',
+  //   'steph.roberts@centrica.com',
+  //   'maya.kamourieh@britishgas.co.uk',
+  //   'evansdeano@hotmail.com',
+];
