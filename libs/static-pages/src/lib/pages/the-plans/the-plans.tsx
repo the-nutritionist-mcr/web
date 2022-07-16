6b3672b5531c55fde3e8ccba660dfea01aaa3686
ken Tsang <hello@kentsang.me>
2022-07-16 17:33:38 +0100

    Update the-plans.tsx

--- libs/static-pages/src/lib/pages/the-plans/the-plans.tsx
+++ libs/static-pages/src/lib/pages/the-plans/the-plans.tsx
@@ -1,1 +1,19 @@
-export const ThePlans = () => <div>Hello, World!</div>;
+export const ThePlans = () => (
+  <div className="the-plans tnm-v1">
+    <main className="main">
+      <section className="page-hero">
+        <div className="page-header">
+          <div className="header-text-block">
+            <h1>
+              We have the <span className="block">perfect plan</span>{' '}
+              <span className="block">for you </span>
+            </h1>
+          </div>
+        </div>
+        <div className="page-header-right">
+          <div className="dish-block"></div>
+        </div>
+      </section>
+    </main>
+  </div>
+);
