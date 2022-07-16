bf515f1303ea4836129b01f7c951524b80af4da1
ken Tsang <hello@kentsang.me>
2022-07-16 17:32:19 +0100

    Update why-choose-us.tsx

--- libs/static-pages/src/lib/pages/why-choose-us/why-choose-us.tsx
+++ libs/static-pages/src/lib/pages/why-choose-us/why-choose-us.tsx
@@ -1,1 +1,19 @@
-export const WhyChooseUs = () => <div>Hello, World!</div>;
+import TNMLogo from '../../../styles/images/svg/TNM_Sticker4_DarkGrey+Yellow.svg';
+
+export const WhyChooseUs = () => (
+  <div className="why-choose-us tnm-v1">
+    <main className="main">
+      <section className="page-hero">
+        <div className="page-header">
+          <div className="header-text-block">
+            <h1>Why Choose Us?</h1>
+          </div>
+        </div>
+        <div className="page-header-right"></div>
+        <div className="hero-stamp">
+          <img src={TNMLogo} alt="" className="image" />
+        </div>
+      </section>
+    </main>
+  </div>
+);
