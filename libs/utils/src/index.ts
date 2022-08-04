6d64d350afb97d939fc845e4fa5ed3a8cd01d097
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-08-04 19:48:36 +0100

    build: fix compile

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,6 +1,6 @@
 export { convertPlanFormat } from './lib/convert-plan-format';
 export { NavigationContext } from './lib/navigation-context';
-export { NavigationContextType } from './lib/navigation-context';
+export type { NavigationContextType } from './lib/navigation-context';
 export { generateDatestampedFilename } from './lib/generate-date-stamped-filename';
 export { updateDelivery } from './lib/update-plan';
 export { isActive } from './lib/is-active';
