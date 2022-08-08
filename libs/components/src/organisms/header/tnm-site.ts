6ad8daf6a55de7fc0ee4e04f4dea40238c21b6d2
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-08-08 07:51:00 +0100

    fix: put staging links back in staging

--- libs/components/src/organisms/header/tnm-site.ts
+++ libs/components/src/organisms/header/tnm-site.ts
@@ -1,4 +1,4 @@
 export const TNM_SITE =
-  process.env.NX_APP_ENV === 'production'
+  process.env.NX_APP_ENV === 'prod'
     ? 'https://www.thenutritionistmcr.com'
     : 'https://staging.thenutritionistmcr.com';
