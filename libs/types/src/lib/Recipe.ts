16c40b0f0082defc49011b6c656a34a0984dc5b7
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-08-13 09:46:46 +0100

    chore: make errors more specific

--- libs/types/src/lib/Recipe.ts
+++ libs/types/src/lib/Recipe.ts
@@ -29,25 +29,25 @@ export const assertIsRecipe: (recipe: unknown) => asserts recipe is Recipe = (
   recipe
 ) => {
   if (typeof recipe !== 'object') {
-    throw new Error('Recipe is not object');
+    throw new TypeError('Recipe is not object');
   }
 
   const asRecipe = recipe as Recipe;
 
   if (typeof asRecipe.id !== 'string') {
-    throw new Error('Recipe.id is not a string');
+    throw new TypeError('Recipe.id is not a string');
   }
 
   if (typeof asRecipe.name !== 'string') {
-    throw new Error('Recipe.name is not a string');
+    throw new TypeError('Recipe.name is not a string');
   }
 
   if (typeof asRecipe.shortName !== 'string') {
-    throw new Error('Recipe.shortName is not a string');
+    throw new TypeError('Recipe.shortName is not a string');
   }
 
   if (typeof asRecipe.hotOrCold !== 'string') {
-    throw new Error('Recipe.hotOrCold is not a string');
+    throw new TypeError('Recipe.hotOrCold is not a string');
   }
 
   if (
@@ -58,29 +58,29 @@ export const assertIsRecipe: (recipe: unknown) => asserts recipe is Recipe = (
         typeof item.alternateId === 'string'
     )
   ) {
-    throw new Error('Recipe.alternates are invalid');
+    throw new TypeError('Recipe.alternates are invalid');
   }
 
   if (
     !Array.isArray(asRecipe.invalidExclusions) ||
     !asRecipe.invalidExclusions.every((item) => typeof item === 'string')
   ) {
-    throw new Error('Recipe.invalidExclusions are invalid');
+    throw new TypeError('Recipe.invalidExclusions are invalid');
   }
 
   if (asRecipe.description && typeof asRecipe.description !== 'string') {
-    throw new Error('Recipe.description is not a string');
+    throw new TypeError('Recipe.description is not a string');
   }
 
   if (asRecipe.allergens && typeof asRecipe.allergens !== 'string') {
-    throw new Error('Recipe.allergens is not a string');
+    throw new TypeError('Recipe.allergens is not a string');
   }
 
   if (
     !Array.isArray(asRecipe.potentialExclusions) ||
     !asRecipe.potentialExclusions.every((item) => isExclusion(item))
   ) {
-    throw new Error('Recipe.potentialExclusions are not valid');
+    throw new TypeError('Recipe.potentialExclusions are not valid');
   }
 
   if (
@@ -88,14 +88,14 @@ export const assertIsRecipe: (recipe: unknown) => asserts recipe is Recipe = (
     (!Array.isArray(asRecipe.invalidExclusions) ||
       !asRecipe.invalidExclusions.every((item) => typeof item === 'string'))
   ) {
-    throw new Error('Recipe.invalidExclusions are not valid');
+    throw new TypeError('Recipe.invalidExclusions are not valid');
   }
 
   if (asRecipe.createdAt && typeof asRecipe.createdAt !== 'string') {
-    throw new Error('Recipe.createdAt is not a string');
+    throw new TypeError('Recipe.createdAt is not a string');
   }
 
   if (asRecipe.updatedAt && typeof asRecipe.createdAt !== 'string') {
-    throw new Error('Recipe.updatedAt is not a string');
+    throw new TypeError('Recipe.updatedAt is not a string');
   }
 };
