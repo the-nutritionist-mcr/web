dfff7aaa42977aa11af5541435b2466de43f915e
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-10-04 18:22:10 +0100

    fix: stuff

--- apps/web-app/src/pages/pricing.page.tsx
+++ apps/web-app/src/pages/pricing.page.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import { Pricing as PricingComponent } from '@tnmw/static-pages';
 
 const OurStory: FC = () => {
-  return <PricingComponent />;
+  return process.env['APP_VERSION'] === 'prod' ? <PricingComponent /> : <></>;
 };
 
 export default OurStory;
