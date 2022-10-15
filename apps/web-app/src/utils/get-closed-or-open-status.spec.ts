417fcc599bf46c187d6e321743b65616819c0889
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-10-15 15:45:34 +0100

    build: fix failing tests

--- apps/web-app/src/utils/get-closed-or-open-status.spec.ts
+++ apps/web-app/src/utils/get-closed-or-open-status.spec.ts
@@ -1,3 +1,5 @@
+import { BackendCustomer, PlannedCook, StandardPlan } from '@tnmw/types';
+import { mock } from 'jest-mock-extended';
 import { getClosedOrOpenStatus } from './get-closed-or-open-status';
 
 const date = (day: number, month: number, year: number, hour: number) => {
@@ -10,24 +12,40 @@ const date = (day: number, month: number, year: number, hour: number) => {
   return date;
 };
 
+const customer = mock<BackendCustomer>({
+  plans: [
+    mock<StandardPlan>({
+      subscriptionStatus: 'active',
+      pauseEnd: undefined,
+      pauseStart: undefined,
+    }),
+  ],
+});
+
+const cooks = [
+  mock<PlannedCook>({
+    date: date(26, 7, 2022, 12),
+  }),
+];
+
 describe('get closed or open status', () => {
   it.each`
-    nowDay | nowMonth | nowYear | nowHour | data                                                                                                           | open
-    ${30}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12) }, published: true, currentUserSelection: {} }}  | ${false}
-    ${30}  | ${7}     | ${2022} | ${11}   | ${undefined}                                                                                                   | ${false}
-    ${25}  | ${7}     | ${2022} | ${11}   | ${undefined}                                                                                                   | ${false}
-    ${27}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12) }, published: true, currentUserSelection: {} }}  | ${true}
-    ${27}  | ${7}     | ${2022} | ${14}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12) }, published: true, currentUserSelection: {} }}  | ${false}
-    ${26}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12) }, published: true, currentUserSelection: {} }}  | ${true}
-    ${26}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12) }, published: false, currentUserSelection: {} }} | ${false}
-    ${25}  | ${7}     | ${2022} | ${17}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12) }, published: true, currentUserSelection: {} }}  | ${true}
-    ${25}  | ${7}     | ${2022} | ${17}   | ${{ available: true, plan: { createdOn: date(18, 7, 2022, 12) }, published: true, currentUserSelection: {} }}  | ${false}
+    nowDay | nowMonth | nowYear | nowHour | data                                                                                                                            | open
+    ${30}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12), cooks }, published: true, currentUserSelection: {}, customer }}  | ${false}
+    ${30}  | ${7}     | ${2022} | ${11}   | ${undefined}                                                                                                                    | ${false}
+    ${25}  | ${7}     | ${2022} | ${11}   | ${undefined}                                                                                                                    | ${false}
+    ${27}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12), cooks }, published: true, currentUserSelection: {}, customer }}  | ${true}
+    ${27}  | ${7}     | ${2022} | ${14}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12), cooks }, published: true, currentUserSelection: {}, customer }}  | ${false}
+    ${26}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12), cooks }, published: true, currentUserSelection: {}, customer }}  | ${true}
+    ${26}  | ${7}     | ${2022} | ${11}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12), cooks }, published: false, currentUserSelection: {}, customer }} | ${false}
+    ${25}  | ${7}     | ${2022} | ${17}   | ${{ available: true, plan: { createdOn: date(25, 7, 2022, 12), cooks }, published: true, currentUserSelection: {}, customer }}  | ${true}
+    ${25}  | ${7}     | ${2022} | ${17}   | ${{ available: true, plan: { createdOn: date(18, 7, 2022, 12), cooks }, published: true, currentUserSelection: {}, customer }}  | ${false}
   `(
     `When date is $nowDay/$nowMonth/$nowYear:$nowHour and data is $data, result should be $open`,
     ({ nowDay, nowMonth, nowYear, nowHour, data, open }) => {
       const now = date(nowDay, nowMonth, nowYear, nowHour);
 
-      const result = getClosedOrOpenStatus(now, data);
+      const result = getClosedOrOpenStatus(now, data, customer);
 
       expect(result).toEqual(open);
     }
