64048e553ed17f4a1fe23fd14f69393025f79e88
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-10-24 21:08:17 +0100

    fix: add missing pagination

---
+++ apps/web-app/src/backend/lambdas/data-api/get-data/batch-get.ts
@@ -0,0 +1,32 @@
+import {
+  BatchGetCommand,
+  BatchGetCommandInput,
+  BatchGetCommandOutput,
+  DynamoDBDocumentClient,
+} from '@aws-sdk/lib-dynamodb';
+
+export const batchGet = async (
+  client: DynamoDBDocumentClient,
+  input: BatchGetCommandInput
+): Promise<BatchGetCommandOutput> => {
+  const command = new BatchGetCommand(input);
+
+  const response = await client.send(command);
+
+  if (response.UnprocessedKeys) {
+    const nextResponse = await batchGet(client, {
+      RequestItems: response.UnprocessedKeys,
+    });
+
+    return {
+      Responses: Object.fromEntries(
+        Object.entries(response.Responses ?? {}).map(([key, value]) => {
+          return [key, [...value, ...(nextResponse.Responses?.[key] ?? [])]];
+        })
+      ),
+      ...response,
+    };
+  }
+
+  return response;
+};
