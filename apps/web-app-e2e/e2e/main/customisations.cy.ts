8bf3d4ce985f1a9aff2d47600bbc6b9c64631d60
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-11-06 12:21:05 +0000

    build: added e2e tests for customisations

--- apps/web-app-e2e/e2e/main/customisations.cy.ts
+++ apps/web-app-e2e/e2e/main/customisations.cy.ts
@@ -1,5 +1,9 @@
 import { E2E } from '@tnmw/constants';
-import { Customisations } from '../../src/pages/customisations';
+import {
+  ConfirmDeleteDialog,
+  CreateCustomisationsDialog,
+  Customisations,
+} from '../../src/pages/customisations';
 
 describe('The customisations page', { scrollBehavior: false }, () => {
   beforeEach(() => {
@@ -14,5 +18,58 @@ describe('The customisations page', { scrollBehavior: false }, () => {
   it("Should load a page titled 'customisations'", () => {
     Customisations.visit();
     Customisations.getHeader();
+  });
+
+  it('should contain table with 111 rows and a header row', () => {
+    Customisations.visit();
+    Customisations.getTableRows().should('have.length', 112);
+  });
+
+  it('Should pop up a dialog when you click new', () => {
+    Customisations.visit();
+    Customisations.clickNewButton();
+    CreateCustomisationsDialog.getCreateDialog();
+  });
+
+  it('Should add an extra row in the recipes table when you fill in the Create Dialog', () => {
+    Customisations.visit();
+    Customisations.clickNewButton();
+    CreateCustomisationsDialog.getCreateDialog();
+    CreateCustomisationsDialog.editNameField('some-allergen');
+    CreateCustomisationsDialog.editAllergenField(true);
+    CreateCustomisationsDialog.clickOk();
+    Customisations.getTable().contains('some-allergen');
+  });
+
+  it('Added recipe should persist across pageload', () => {
+    Customisations.visit();
+    Customisations.getTable().contains('some-allergen');
+  });
+
+  it('Added customisation should have all its data correctly persisted', () => {
+    Customisations.visit();
+    Customisations.clickCustomisationEdit('some-allergen');
+    Customisations.getNameField().should('have.value', 'some-allergen');
+    Customisations.getAllergenField().should('be.checked');
+  });
+
+  it('Should persist changes to customisations', () => {
+    Customisations.visit();
+    Customisations.clickCustomisationEdit('No Balsam');
+    CreateCustomisationsDialog.editNameField('Actually Balsam');
+    CreateCustomisationsDialog.editAllergenField(true);
+    CreateCustomisationsDialog.clickOk();
+    Customisations.getTable().contains('Actually Balsam');
+    Customisations.visit();
+    Customisations.clickCustomisationEdit('Actually Balsam');
+    Customisations.getNameField().should('have.value', 'Actually Balsam');
+    Customisations.getAllergenField().should('be.checked');
+  });
+
+  it('Should remove a row when you click the delete button and confirm', () => {
+    Customisations.visit();
+    Customisations.clickCustomisationDelete('some-allergen');
+    ConfirmDeleteDialog.clickOk();
+    Customisations.getTable().contains('some-allergen').should('not.exist');
   });
 });
--- apps/web-app-e2e/src/pages/customisations.ts
+++ apps/web-app-e2e/src/pages/customisations.ts
@@ -3,7 +3,46 @@ export const Customisations = {
     cy.visit('/admin/customisations');
   },
 
-  getHeader: () => {
-    cy.contains('h2', 'Customisations');
+  getHeader: () => cy.contains('h2', 'Customisations'),
+  getTable: () => cy.get('table'),
+
+  getTableRows: () => cy.get('table').find('tr'),
+
+  clickNewButton: () => cy.contains('button', 'New').click(),
+  getNameField: () => cy.get('form').find(`input[name='name']`),
+  getAllergenField: () => cy.get('form').find(`input[name='allergen']`),
+
+  clickCustomisationDelete: (name: string) =>
+    cy
+      .contains(name)
+      .parent('tr')
+      .find('button[aria-label="Delete"]')
+      .click({ force: true }),
+
+  clickCustomisationEdit: (name: string) =>
+    cy
+      .contains(name)
+      .parent('tr')
+      .find('button[aria-label="Edit"]')
+      .click({ force: true }),
+};
+
+export const ConfirmDeleteDialog = {
+  clickOk: () => cy.contains('button', 'Ok').click(),
+  clickCancel: () => cy.contains('button', 'Cancel').click(),
+};
+
+export const CreateCustomisationsDialog = {
+  getCreateDialog: () => cy.get('form'),
+
+  editNameField: (text: string) =>
+    cy.get('form').find(`input[name='name']`).clear().type(text),
+
+  editAllergenField: (isAllergen: boolean) => {
+    return isAllergen
+      ? cy.get('form').find(`input[name='allergen']`).check({ force: true })
+      : cy.get('form').find(`input[name='allergen']`).uncheck({ force: true });
   },
+
+  clickOk: () => cy.get('form').contains('button', 'Ok').click({ force: true }),
 };
