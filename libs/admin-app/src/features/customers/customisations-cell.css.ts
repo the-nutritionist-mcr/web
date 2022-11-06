adef97cf944b6a5d727defada449745d2c07fd49
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-11-06 12:22:09 +0000

    build: added missing css file

---
+++ libs/admin-app/src/features/customers/customisations-cell.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const noPlan = style({
+  color: '#ECECEC',
+});
+
+export const container = style({
+  display: 'flex',
+  gap: '0.2rem',
+  flexWrap: 'wrap',
+  margin: '0.2rem',
+});
