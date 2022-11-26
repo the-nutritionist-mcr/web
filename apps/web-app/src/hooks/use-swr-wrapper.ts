7889cb7f0a55e5108b5ceb8207c36d40db3c0fa3
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-11-26 02:11:54 +0000

    chore: remove dead code

--- apps/web-app/src/hooks/use-swr-wrapper.ts
+++ apps/web-app/src/hooks/use-swr-wrapper.ts
@@ -1,22 +1,10 @@
 import { LoadingContext } from '@tnmw/components';
 import { recursivelyDeserialiseDate, SerialisedDate } from '@tnmw/utils';
 import { useContext } from 'react';
-import useSWR, { Key, Middleware } from 'swr';
+import useSWR from 'swr';
 import { swrFetcher } from '../utils/swr-fetcher';
 
 type ParamsType<T> = Parameters<typeof useSWR<SerialisedDate<T>>>;
-
-const doStartLoading = (key: Key) => {
-  if (typeof key === 'function') {
-    try {
-      key();
-    } catch {
-      return false;
-    }
-  }
-
-  return Boolean(key);
-};
 
 export const useSwrWrapper = <T = unknown>(
   key: ParamsType<T>[0],
@@ -38,11 +26,6 @@ export const useSwrWrapper = <T = unknown>(
 
   type OnErrorType = Exclude<
     Exclude<typeof options, undefined>['onError'],
-    undefined
-  >;
-
-  type OnSuccessType = Exclude<
-    Exclude<typeof options, undefined>['onSuccess'],
     undefined
   >;
 
