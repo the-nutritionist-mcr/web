544b11380354e044f9d6c6ee8371def69062fbf6
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-02-15 19:58:25 +0000

    chore: remove erroneous tests

--- apps/web-app-e2e/e2e/initial/login.cy.ts
+++ apps/web-app-e2e/e2e/initial/login.cy.ts
@@ -35,34 +35,4 @@ describe('The login page', { scrollBehavior: false }, () => {
 
     LoginPage.getLoginForm().contains('Incorrect username or password');
   });
-
-  it('Creating an account on ChargeBee should result in being sent a password that I can use to login and change my password with', () => {
-    cy.task('createChargebeeCustomer', E2E.nonExistingUser);
-
-    cy.task('getPasswordFromWelcomeEmailThenDelete').then(
-      (password: string) => {
-        LoginPage.visit();
-        LoginPage.getLoginForm().should('exist');
-        LoginPage.fillEmailInput(E2E.nonExistingUser.email);
-
-        LoginPage.fillPasswordInput(password);
-        LoginPage.clickLoginButton();
-        LoginPage.getSubmitButton().should('exist');
-        LoginPage.fillPasswordInput(E2E.nonExistingUser.password);
-        LoginPage.clickSubmitButton();
-
-        AccountPage.isInNavbar();
-      }
-    );
-  });
-
-  it('redirects you straight to the account page next time you login once the password has been changed', () => {
-    LoginPage.visit();
-    LoginPage.getLoginForm().should('be.visible');
-    LoginPage.fillEmailInput(E2E.nonExistingUser.email);
-    LoginPage.fillPasswordInput(E2E.nonExistingUser.password);
-
-    LoginPage.clickLoginButton();
-    AccountPage.isInNavbar();
-  });
 });
