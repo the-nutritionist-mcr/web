52a3cf4340185bf8adf22751faafc1384d52b160
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-03 12:15:11 +0100

    fix: revert returnOkResponse

--- apps/web-app/src/backend/lambdas/data-api/return-ok-response.ts
+++ apps/web-app/src/backend/lambdas/data-api/return-ok-response.ts
@@ -1,18 +1,12 @@
 import { HTTP } from '@tnmw/constants';
 import { allowHeaders } from '../../allow-headers';
 
-const allowedOrigins = [
-  `https://api.quickbudget.co.uk`,
-  `http://localhost:4200`,
-  `http://192.168.1.102:4200`,
-];
-
 export const returnOkResponse = <T>(body: T) => {
   return {
     statusCode: HTTP.statusCodes.Ok,
     body: JSON.stringify(body),
     headers: {
-      [HTTP.headerNames.AccessControlAllowOrigin]: allowedOrigins.join(', '),
+      [HTTP.headerNames.AccessControlAllowOrigin]: '*',
       [HTTP.headerNames.AccessControlAllowHeaders]: allowHeaders.join(', '),
     },
   };
