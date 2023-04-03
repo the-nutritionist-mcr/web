2710946690f7d6989394c4f65aad90dc45798cc6
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-03 13:08:30 +0100

    fix: put push back for now

--- apps/web-app/src/pages/_app.page.tsx
+++ apps/web-app/src/pages/_app.page.tsx
@@ -25,8 +25,8 @@ import { ErrorFallback } from '../components/error-fallback';
 const navigator = {
   navigate: async (path: string, withRouter = true) => {
     // eslint-disable-next-line fp/no-mutating-methods
-    // await Router.push(path);
-    window.location.href = path
+    await Router.push(path);
+    // window.location.href = path
   },
   prefetch: (path: string) => {
     Router.prefetch(path);
